050b8e913c956d7416165dfa40ec26a08d8fd782
azimi <azimiraviz.mr@gmail.com>
2025-09-15 23:17:41 +0330

    initialize project using spring initializer

---
+++ src/main/java/ir/azimi/labs/java/JavaLabsApplication.java
@@ -0,0 +1,10 @@
+package ir.azimi.labs.java;
+
+@SpringBootApplication
+public class JavaLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLabsApplication.class, args);
+	}
+
+}
